a0e2e2ce46fe945da7ba8ff4fb6c4f940c4f8071
Ryan Geary <rtgnj42@gmail.com>
2025-07-23 21:28:01 -0400

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import { useState } from 'react'
 import headshot from '/headshot.jpg'
 import './App.css'
 
 function App() {
   return (
     <>
-        <div class="container mx-auto px-4">
-            <h1 class="text-3xl font-bold underline p-4 text-violet-900/50">
+        <div className="container mx-auto px-4">
+            <h1 className="text-3xl font-bold underline p-4 text-violet-900/50">
                 Ryan Geary
             </h1>
 
-            <div class="flex justify-center">
-                <img src={headshot} alt="a pic of me" class="w-3xs rounded-full p-10 " />
+            <div className="flex justify-center">
+                <img src={headshot} alt="a pic of me" className="w-3xs rounded-full p-10 " />
             </div>
 
-            <div class="grid grid-cols-2 grid-rows-2 gap-4">
-                <a href="/projects" class="btn">Projects</a>
-                <a href="/blog" class="btn">Blog</a>
-                <a target="_blank" href="https://github.com/theryangeary/" class="btn">Github</a>
-                <a target="_blank" href="https://www.linkedin.com/in/theryangeary/" class="btn">LinkedIn</a>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4">
+                <a href="/projects" className="btn">Projects</a>
+                <a href="/blog" className="btn">Blog</a>
+                <a target="_blank" href="https://github.com/theryangeary/" className="btn">Github</a>
+                <a target="_blank" href="https://www.linkedin.com/in/theryangeary/" className="btn">LinkedIn</a>
             </div>
         </div>
     </>
