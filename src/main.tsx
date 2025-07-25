b507a8abf9a73d3ae2a4e5d8217fdf58439b555d
Ryan Geary <rtgnj42@gmail.com>
2025-07-25 00:00:00 +0000

    add buffer

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Buffer } from 'buffer'
 import './index.css'
 import App from './App.tsx'
+
+// Polyfill Buffer for browser compatibility
+(window as any).Buffer = Buffer
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
